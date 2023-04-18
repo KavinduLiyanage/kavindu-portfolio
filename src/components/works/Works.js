9a695e7816146aa018d13c7035d0edf64782dead
KavinduLiyanage <sandeepa8956@gmail.com>
2023-04-18 19:24:34 +0530

    Updated about me section

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -5,17 +5,17 @@ import { Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 
-import './Works.css';
+import "./Works.css";
 
-import Portfolio from '../../assets/recentprojects/react-portfolio.png';
-import Homaze from '../../assets/recentprojects/Homaze.png';
-import Workwise from '../../assets/recentprojects/Workwise.png';
-import FusionX from '../../assets/recentprojects/Lending.png';
+import Portfolio from "../../assets/recentprojects/react-portfolio.png";
+import Homaze from "../../assets/recentprojects/Homaze.png";
+import Workwise from "../../assets/recentprojects/Workwise.png";
+import FusionX from "../../assets/recentprojects/Lending.png";
 
 const useStyles = makeStyles((theme) => ({
   main: {
-    maxWidth: '100vw',
-    marginTop: '3em',
+    maxWidth: "100vw",
+    marginTop: "3em",
     marginBottom: "auto",
   },
 }));
@@ -25,36 +25,36 @@ export const Works = () => {
   const [projects, setProjects] = useState([
     {
       id: 1,
-      title: 'Workwise Web App',
+      title: "Workwise Web App",
       description: `Workwise is an online marketplace that connects people who want to rent out their properties, office listing with people who are looking for properties in that locale. For owners and affiliates participating in Workwise is a way to earn income from their property.`,
-      technologies: `ReactJS, Redux, Redux-saga, Google API, Material UI, NodeJS, ExpressJS, MongoDB, AWS EC2, S3, Formik`,
-      alter: 'Startup Project',
+      technologies: `React-TypeScript, Redux, Redux-saga, Google API, Material UI, Node, Express, MongoDB, AWS EC2, S3, Formik`,
+      alter: "Startup Project",
       image: `${Workwise}`,
     },
     {
       id: 2,
-      title: 'Homaze Web App',
+      title: "Homaze Web App",
       description: `Homaze is an Online Quotation Generating Web app that helps remodelers & renovation experts create estimates in minutes on-site, turn those estimates into professional-grade proposals, and share them with their clients. Remodelers can manage all client communication in one place. Homaze uses technology that turns the tedious remodeling office work process into an easy-to-use tool.`,
-      technologies: `React with Typescript, Redux, Redux-saga, Material UI, NodeJS, ExpressJS, MongoDB, AWS EC2, S3, Formik`,
-      alter: 'Startup Project',
+      technologies: `ReactJS, Redux, Redux-Saga, Material-UI, SASS, ChartJS, Node, Express, MongoDB, AWS EC2, S3, Formik`,
+      alter: "Startup Project",
       image: `${Homaze}`,
     },
     {
       id: 3,
-      title: 'FusionX Banking App',
+      title: "FusionX Banking App",
       description: `FusionX is a core-banking platform developed to help standardize and optimize the operations of the branches.`,
-      technologies: `React with TypeScript, Redux-toolkit, Ant-Design, Tailwind CSS, Styled-components, Auth0`,
-      alter: 'Banking System',
+      technologies: `React-TypeScript, Redux-toolkit, Ant-Design, Tailwind CSS, Auth0, Redux-Thunk, SASS, Styled-components, Recharts`,
+      alter: "Banking System",
       image: `${FusionX}`,
     },
     {
       id: 4,
-      title: 'Portfolio Web App',
+      title: "Portfolio Web App",
       description: `Designed and developed a ReactJS portfolio 
       with fancy 3D animations using Three.js for 
       the background element.`,
       technologies: `ReactJS, Material-ui`,
-      alter: 'React Portfolio',
+      alter: "React Portfolio",
       image: `${Portfolio}`,
     },
   ]);
@@ -69,11 +69,11 @@ export const Works = () => {
             </div>
             <div className="__content_wrapper">
               <h3 className="title">
-                <TextDecrypt text={project.id + '. ' + project.title} />
+                <TextDecrypt
+                  text={project.id + ". " + project.title}
+                />
               </h3>
-              <p className="description">
-                {project.description}
-              </p>
+              <p className="description">{project.description}</p>
               <p className="technologies">
                 Technologies: {project.technologies}
               </p>
